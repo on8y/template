62a8a1f94fadb5ba3f3ceddf92d424c234c61503
wwwmewww <wwwmewww@163.com>
2020-09-01 14:09:11 +0800

    提交 用 graphgl 添加数据

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,44 @@
 import { MikroORM } from '@mikro-orm/core';
 import microConfig from './mikro-orm.config';
-import { Post } from './entities/Post';
+// import { Post } from './entities/Post';
+
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
+
+import 'reflect-metadata';
 
 const main = async () => {
+	//ORM
 	const orm = await MikroORM.init(microConfig);
 	await orm.getMigrator().up();
+
 	// cosnt post = orm.em.create(Post, {title: 'my first post'});
 	// await orm.em.persistAndFlush(post);
 	// console.log('--------------sql 2--------------');
 	// await orm.em.nativeInsert(Post, { title: 'my first post 2'});
+	// const posts = await orm.em.find(Post, {});
+	// console.log(posts);
 
-	const posts = await orm.em.find(Post, {});
-	console.log(posts);
+	//web server
+	const app = express();
+	const apolloServer = new ApolloServer({
+		schema: buildSchema({
+			resolvers: [HelloResolver, PostResolver],
+			validate: false
+		}),
+		context: (/* {req, res}*/) => ({ em: orm.em })
+	});
+	apolloServer.applyMiddleware({ app });
+
+	// app.get('/', (_, res) => {
+	// 	res.send('hello world');
+	// });
+	app.listen(5000, () => {
+		console.log('server started on http://localhost:5000');
+	});
 };
 
 main().catch(err => {
