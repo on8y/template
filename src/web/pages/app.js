3d02a9a65e12d483b9880c0cf50458ae6746eba0
wwwmewww <wwwmewww@163.com>
2020-09-04 22:48:13 +0800

    提交

--- src/web/pages/app.js
+++ src/web/pages/app.js
@@ -1,13 +1,13 @@
-import React from "react";
-import Home from "./home";
+import React from 'react';
+import Home from './home';
 
 function App() {
-  return (
-    <div>
-      <h1>App Page</h1>
-      <Home />
-    </div>
-  );
+	return (
+		<div>
+			<h1>App Page</h1>
+			<Home />
+		</div>
+		)
 }
 
 export default App;
